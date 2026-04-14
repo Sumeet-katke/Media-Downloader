124fe3a18f85f2c8799474a5856ecca2dfac1f42
Sumeetkumar katke <snkatke9874@gmail.com>
2026-04-15 00:41:20 +0530

    Update server run configuration to use environment variables for host and port

--- AMD/app.py
+++ AMD/app.py
@@ -55,4 +55,7 @@ def download_video(url):
 
 # 8. Run server
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
